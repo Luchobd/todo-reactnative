d9d540d3c7c5cee0ce14ac99a13dbf7c1886fbbd
Luis Blanco <luiswblancod@gmail.com>
2023-07-25 08:31:43 -0500

    create login and asyncstorage

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { User } from "../interfaces/user.interface";
+import { Login } from "../interfaces/auth.interface";
 
 const API = "http://10.0.2.2:3000/auth";
 
 export const registerRequest = async (user: User) => await axios.post(`${API}/register`, user);
+export const loginRequest = async (auth: Login) => await axios.post(`${API}/login`, auth);
 
